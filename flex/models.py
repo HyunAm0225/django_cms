dbb7caeb6996a0eadf183da70a388a3b3106d789
HyunAm0225 <41044429+HyunAm0225@users.noreply.github.com>
2021-12-13 17:36:01 +0900

    Feat : stream field 추가

--- flex/models.py
+++ flex/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
+
+
+from streams import blocks
 
 # Create your models here.
 
@@ -8,8 +12,18 @@ from wagtail.core.models import Page
 class FlexPage(Page):
     template = "flex/flex_page.html"
     subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
     ]
 
     class Meta:
