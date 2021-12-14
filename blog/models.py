ff35e95d57b4e7cc905ed517647f2e9ffcbf0c16
HyunAm0225 <41044429+HyunAm0225@users.noreply.github.com>
2021-12-14 15:24:42 +0900

    Fix : blog page

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
+from streams import blocks
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=True,
+        help_text="Overwrites the default title",
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+        FieldPanel("custom_title"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["posts"] = BlogDetailPage.objects.live().public()
+        return context
 
 
 class BlogPage(Page):
@@ -26,4 +42,39 @@ class BlogPage(Page):
         FieldPanel("date"),
         FieldPanel("intro"),
         FieldPanel("body", classname="full"),
+    ]
+
+
+class BlogDetailPage(Page):
+    """blog detail page"""
+
+    custom_title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=True,
+        help_text="Overwrites the default title",
+    )
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=False,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichTextblock()),
+            ("simple_richtext", blocks.SimpleRichTextblock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("custom_title"),
+        ImageChooserPanel("blog_image"),
+        StreamFieldPanel("content"),
     ]
