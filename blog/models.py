7b25f2182f89d5a06e55b0c6fb8b13ab2db9c8c1
HyunAm0225 <41044429+HyunAm0225@users.noreply.github.com>
2021-12-14 19:03:35 +0900

    Feat : blog author 추가(snippets)

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,53 @@ from streams import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.contrib.routable_page.models import RoutablePage, RoutablePageMixin, route
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.snippets.models import register_snippet
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+
+
+class BlogAuthor(models.Model):
+    """blog author for snippets."""
+
+    name = models.CharField(
+        max_length=100,
+    )
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+",
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("name"),
+                ImageChooserPanel("image"),
+            ],
+            heading="Name and Image",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("website"),
+            ],
+            heading="Links",
+        ),
+    ]
+
+    def __str__(self):
+        """string repr of this class"""
+        return self.name
+
+    class Meta:  # noqa
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
+
+
+register_snippet(BlogAuthor)
 
 
 class BlogIndexPage(RoutablePageMixin, Page):
